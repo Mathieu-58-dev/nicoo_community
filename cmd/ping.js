8752e4a78c1cb7fbaaf69dc9d2b3b2bbb59b11dc
Mathieu-58-dev <41152354+Mathieu-58-dev@users.noreply.github.com>
2018-08-29 13:58:55 +0200

    Add files via upload

---
+++ cmd/ping.js
@@ -0,0 +1,10 @@
+module.exports.run = async (bot, prefix, args, msg, flash) => {
+
+    msg.channel.send(":ping_pong: Pong").then(message => {
+        message.edit(`:ping_pong: Pong | ${message.createdTimestamp-msg.createdTimestamp} ms`);
+    }).catch(console.error);
+}
+
+module.exports.help = {
+    name: "ping"
+}
