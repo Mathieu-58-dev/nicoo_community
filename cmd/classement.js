da6ad070fb3e8a185e9fbb853f551f2e1c5052f6
Mathieu-58-dev <41152354+Mathieu-58-dev@users.noreply.github.com>
2018-09-01 15:36:00 +0200

    Delete classement.js

--- cmd/classement.js
+++
@@ -1,3 +0,0 @@
-module.exports.run = async (bot, prefix, args, msg, flash) => {
-    msg.reply ("Le classement des membres les plus bavards du discord : https://mee6.xyz/leaderboard/277543113302736898")
-}
