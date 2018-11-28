97f8def0f987dd54535841ec0bf04aca4253f6f6
Mathieu-58-dev <41152354+Mathieu-58-dev@users.noreply.github.com>
2018-11-28 16:56:31 +0100

    Delete ping.js

--- cmd/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports.run = async (Discord, bot, prefix, args, msg, flash, créateurbot, fortnite) => {
-
-    msg.channel.send(":ping_pong: Pong").then(message => {
-        message.edit(`:ping_pong: Pong | ${message.createdTimestamp-msg.createdTimestamp} ms`);
-    }).catch(console.error);
-}
